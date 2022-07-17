705e569a4ed2e3ecf7528fcc1079bd3e05ebb78b
Arthur Silveira <78749692+ArthurSSan@users.noreply.github.com>
2022-07-17 20:56:21 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GloblaStyle } from "./styles/global";
+import { TransactionsContext, TransactionsProvider } from './hooks/useTransactions';
+
+export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Dashboard/>
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GloblaStyle/>
+      </TransactionsProvider>
+  );
+}
